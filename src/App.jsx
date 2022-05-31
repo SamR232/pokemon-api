966bddfef491a016dbc8b8893b92f8abea1ec5a1
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-31 15:22:52 +0100

    configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import PokeReq from './Wrapper';
 
+import './index.css';
+
 // declare function to be used in getGenerationOne func
 const getIndividuaLPokemon = async (name) => {
 	const individualPokemonData = await PokeReq.getPokemonByName(name);
@@ -11,17 +13,18 @@ const getIndividuaLPokemon = async (name) => {
 const getGenerationOne = async () => {
 	const genOnePokemon = await PokeReq.getGenerationByName(1);
 
-	const names = genOnePokemon.pokemon_species.map((pokemon)=>{
-		return pokemon.name
-	})
+	const names = genOnePokemon.pokemon_species.map((pokemon) => {
+		return pokemon.name;
+	});
 
-	const res = await Promise.all(names.map((name)=>{
-		return getIndividuaLPokemon(name)
-	}))
+	const res = await Promise.all(
+		names.map((name) => {
+			return getIndividuaLPokemon(name);
+		})
+	);
 
 	return res;
 };
-
 
 const App = () => {
 	const [pokemon, setPokemon] = useState([]);
@@ -32,39 +35,43 @@ const App = () => {
 			setPokemon(generationResponse);
 		}
 
-		fetchGenerationOne()
-
+		fetchGenerationOne();
 	}, []);
-	
-	useEffect(()=>{
+
+	useEffect(() => {
 		console.log(pokemon, 'pokemon');
-	}, [pokemon])
+	}, [pokemon]);
 
 	return (
 		<div>
-			<h1>Pokedex</h1>
+			<h1 className='text-2xl text-red-700'>Pokedex</h1>
 
 			<div>
 				<table>
 					<thead>
-					<tr>
-						<th>Name</th>
-						<th>Types</th>
-						<th>Image</th>
-					</tr>
+						<tr>
+							<th>Name</th>
+							<th>Types</th>
+							<th>Image</th>
+						</tr>
 					</thead>
 					<tbody>
-					{pokemon.map((currPokemon) => {
+						{pokemon.map((currPokemon) => {
 							return (
-							<tr>
-							<td>{currPokemon.name}</td>
-							<td>{currPokemon.types.map((type)=>{
-								return type.type.name
-							}).join(', ')
-							} </td>
-							<td><img src={currPokemon.sprites.front_default}></img></td>
-							</tr>
-							)
+								<tr>
+									<td>{currPokemon.name}</td>
+									<td>
+										{currPokemon.types
+											.map((type) => {
+												return type.type.name;
+											})
+											.join(', ')}{' '}
+									</td>
+									<td>
+										<img src={currPokemon.sprites.front_default}></img>
+									</td>
+								</tr>
+							);
 						})}
 					</tbody>
 				</table>
